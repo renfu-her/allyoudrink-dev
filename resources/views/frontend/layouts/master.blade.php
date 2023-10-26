e3e0f972a462d7252fb3c302319d37ee68323d92
Patrick Her <renfu.her@gmail.com>
2023-10-26 14:46:47 +0800

    update custom.css & master

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -10,7 +10,7 @@
     <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
 
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/templatemo.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/templatemo.css?t=' . time()) }}">
     <link rel="stylesheet" href="{{ asset('css/custom.css?t=' . time()) }}">
 
     <!-- Load fonts style after rendering the layout styles -->
@@ -24,10 +24,10 @@
 <body>
 
     <!-- Header -->
-    <nav class="navbar navbar-expand-lg navbar-light">
+    <nav class="navbar-top navbar-expand-lg navbar-light">
         <div class="container d-flex justify-content-between align-items-center">
 
-            <a class="navbar-brand text-success logo h3 align-self-center col-12" href="/">
+            <a class="navbar-brand text-success logo h3 align-self-center" style="width: 95%" href="/">
                 <span class="d-flex justify-content-center">購物網站</span>
             </a>
 
@@ -38,7 +38,54 @@
             </button>
 
         </div>
+        <div class="container">
+            <div class="align-self-center collapse navbar-collapse flex-fill d-lg-flex col-12 justify-content-center justify-content-lg-between"
+                id="templatemo_main_nav">
+                <div class="flex-fill">
+                    <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto ">
+                        <li class="nav-item ">
+                            <a class="nav-link" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="about.html">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="shop.html">Shop</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.html">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="navbar align-self-center d-flex">
+                    <div class="d-lg-none flex-sm-fill mt-3 mb-4 col-7 col-sm-auto pr-3">
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="inputMobileSearch" placeholder="Search ...">
+                            <div class="input-group-text">
+                                <i class="fa fa-fw fa-search"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="nav-icon d-none d-lg-inline" href="#" data-bs-toggle="modal"
+                        data-bs-target="#templatemo_search">
+                        <i class="fa fa-fw fa-search text-dark mr-2"></i>
+                    </a>
+                    <a class="nav-icon position-relative text-decoration-none" href="#">
+                        <i class="fa fa-fw fa-cart-arrow-down text-dark mr-1"></i>
+                        {{-- <span
+                    class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">7</span> --}}
+                    </a>
+                    <a class="nav-icon position-relative text-decoration-none" href="#">
+                        <i class="fa fa-fw fa-user text-dark mr-3"></i>
+                        {{-- <span
+                    class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">+99</span> --}}
+                    </a>
+                </div>
+            </div>
+        </div>
     </nav>
+    <!--
     <nav class="navbar navbar-expand-lg navbar-light d-md-block d-none">
         <div class="container d-flex justify-content-between align-items-center">
 
@@ -88,6 +135,7 @@
 
         </div>
     </nav>
+    -->
     <!-- Close Header -->
 
     <!-- Modal -->
