ce8920c5cd973834320e30b32da6f939a6a2a099
Patrick Her <renfu.her@gmail.com>
2023-10-26 15:47:22 +0800

    migrate

---
+++ database/migrations/2023_06_18_155140_rename_invoice_to_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            // $table->renameColumn('invoice', 'invoice_no');
+            DB::statement("ALTER TABLE orders
+            CHANGE COLUMN invoice invoice_no varchar(255);");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            //
+        });
+    }
+};
