211cd48f3020e206bf31efbcf10b0ad497eb6884
Patrick Her <renfu.her@gmail.com>
2023-10-28 14:24:14 +0800

    update app-login

--- resources/views/backend/layouts/app-login.blade.php
+++ resources/views/backend/layouts/app-login.blade.php
@@ -20,6 +20,7 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
     </script>
+    <script src="{{ asset('admin/js/jquery.min.js') }}"></script>
     <script src="{{ asset('admin/js/scripts.js') }}"></script>
 
     @yield('js')
