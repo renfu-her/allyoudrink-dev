125754056a0ea9a7e3e2e555a766e4e599173193
Patrick Her <renfu.her@gmail.com>
2024-01-06 11:22:52 +0800

    upload images

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -37,7 +37,7 @@
                         <div class="col-md-4">
                             <div class="card mb-4 product-wap rounded-0">
                                 <div class="card rounded-0">
-                                    <img class="card-img rounded-0 img-fluid" src="{{ asset('upload/image/' . $product['id'] . '/' . $product['image']) }}">
+                                    <img class="card-img rounded-0 img-fluid" src="{{ asset('upload/images/' . $product['id'] . '/' . $product['image']) }}">
                                     <div
                                         class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
                                         <ul class="list-unstyled">
