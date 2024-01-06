c907ae5593eac785c2ee8e32527fcd6f2febd1e3
Patrick Her <renfu.her@gmail.com>
2024-01-06 22:16:01 +0800

    update image

--- resources/views/frontend/product/product_detail.blade.php
+++ resources/views/frontend/product/product_detail.blade.php
@@ -30,9 +30,9 @@
                 <div class="col-lg-7 mt-5">
                     <div class="card">
                         <div class="card-body">
-                            <h1 class="h2">Active Wear</h1>
-                            <p class="h3 py-2">$25.00</p>
-                            <p class="py-2">
+                            <h1 class="h2">{{ $product->name }}</h1>
+                            <p class="h3 py-2">$ {{ $product->price }}</p>
+                            {{-- <p class="py-2">
                                 <i class="fa fa-star text-warning"></i>
                                 <i class="fa fa-star text-warning"></i>
                                 <i class="fa fa-star text-warning"></i>
@@ -47,13 +47,11 @@
                                 <li class="list-inline-item">
                                     <p class="text-muted"><strong>Easy Wear</strong></p>
                                 </li>
-                            </ul>
+                            </ul> --}}
 
-                            <h6>Description:</h6>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temp incididunt ut
-                                labore et dolore magna aliqua. Quis ipsum suspendisse. Donec condimentum elementum
-                                convallis. Nunc sed orci a diam ultrices aliquet interdum quis nulla.</p>
-                            <ul class="list-inline">
+                            <h6>產品說明:</h6>
+                            <p>{!! $product->description !!}</p>
+                            {{-- <ul class="list-inline">
                                 <li class="list-inline-item">
                                     <h6>Avaliable Color :</h6>
                                 </li>
@@ -106,15 +104,15 @@
                                                     id="btn-plus">+</span></li>
                                         </ul>
                                     </div>
-                                </div>
+                                </div> --}}
                                 <div class="row pb-3">
                                     <div class="col d-grid">
                                         <button type="submit" class="btn btn-success btn-lg" name="submit"
-                                            value="buy">Buy</button>
+                                            value="buy">購 買</button>
                                     </div>
                                     <div class="col d-grid">
                                         <button type="submit" class="btn btn-success btn-lg" name="submit"
-                                            value="addtocard">Add To Cart</button>
+                                            value="addtocard">加入購物車</button>
                                     </div>
                                 </div>
                             </form>
