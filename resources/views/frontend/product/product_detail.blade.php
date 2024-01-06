2f35d842d090c9ca007ce7ea22eb4f6bbdad0a22
Patrick Her <renfu.her@gmail.com>
2024-01-06 17:00:56 +0800

    update

--- resources/views/frontend/product/product_detail.blade.php
+++ resources/views/frontend/product/product_detail.blade.php
@@ -5,111 +5,11 @@
         <div class="container pb-5">
             <div class="row">
                 <div class="col-lg-5 mt-5">
-                    <div class="card mb-3">
-                        <img class="card-img img-fluid" src="assets/img/product_single_10.jpg" alt="Card image cap"
-                            id="product-detail">
-                    </div>
-                    <div class="row">
-                        <!--Start Controls-->
-                        <div class="col-1 align-self-center">
-                            <a href="#multi-item-example" role="button" data-bs-slide="prev">
-                                <i class="text-dark fas fa-chevron-left"></i>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                        </div>
-                        <!--End Controls-->
-                        <!--Start Carousel Wrapper-->
-                        <div id="multi-item-example" class="col-10 carousel slide carousel-multi-item"
-                            data-bs-ride="carousel">
-                            <!--Start Slides-->
-                            <div class="carousel-inner product-links-wap" role="listbox">
-
-                                <!--First slide-->
-                                <div class="carousel-item active">
-                                    <div class="row">
-                                        <div class="col-4">
-                                            <a href="#">
-                                                <img class="card-img img-fluid" src="assets/img/product_single_01.jpg"
-                                                    alt="Product Image 1">
-                                            </a>
-                                        </div>
-                                        <div class="col-4">
-                                            <a href="#">
-                                                <img class="card-img img-fluid" src="assets/img/product_single_02.jpg"
-                                                    alt="Product Image 2">
-                                            </a>
-                                        </div>
-                                        <div class="col-4">
-                                            <a href="#">
-                                                <img class="card-img img-fluid" src="assets/img/product_single_03.jpg"
-                                                    alt="Product Image 3">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--/.First slide-->
-
-                                <!--Second slide-->
-                                <div class="carousel-item">
-                                    <div class="row">
-                                        <div class="col-4">
-                                            <a href="#">
-                                                <img class="card-img img-fluid" src="assets/img/product_single_04.jpg"
-                                                    alt="Product Image 4">
-                                            </a>
-                                        </div>
-                                        <div class="col-4">
-                                            <a href="#">
-                                                <img class="card-img img-fluid" src="assets/img/product_single_05.jpg"
-                                                    alt="Product Image 5">
-                                            </a>
-                                        </div>
-                                        <div class="col-4">
-                                            <a href="#">
-                                                <img class="card-img img-fluid" src="assets/img/product_single_06.jpg"
-                                                    alt="Product Image 6">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--/.Second slide-->
-
-                                <!--Third slide-->
-                                <div class="carousel-item">
-                                    <div class="row">
-                                        <div class="col-4">
-                                            <a href="#">
-                                                <img class="card-img img-fluid" src="assets/img/product_single_07.jpg"
-                                                    alt="Product Image 7">
-                                            </a>
-                                        </div>
-                                        <div class="col-4">
-                                            <a href="#">
-                                                <img class="card-img img-fluid" src="assets/img/product_single_08.jpg"
-                                                    alt="Product Image 8">
-                                            </a>
-                                        </div>
-                                        <div class="col-4">
-                                            <a href="#">
-                                                <img class="card-img img-fluid" src="assets/img/product_single_09.jpg"
-                                                    alt="Product Image 9">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!--/.Third slide-->
-                            </div>
-                            <!--End Slides-->
-                        </div>
-                        <!--End Carousel Wrapper-->
-                        <!--Start Controls-->
-                        <div class="col-1 align-self-center">
-                            <a href="#multi-item-example" role="button" data-bs-slide="next">
-                                <i class="text-dark fas fa-chevron-right"></i>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </div>
-                        <!--End Controls-->
+                    <div class="slider">
+                        @foreach ($productImage as $image)
+                            <div><img src="{{ asset('upload/images/', $product->id . '/' . $image['image']) }}"
+                                    alt="圖片1"></div>
+                        @endforeach
                     </div>
                 </div>
                 <!-- col end -->
@@ -165,8 +65,7 @@
                                     <div class="col-auto">
                                         <ul class="list-inline pb-3">
                                             <li class="list-inline-item">Size :
-                                                <input type="hidden" name="product-size" id="product-size"
-                                                    value="S">
+                                                <input type="hidden" name="product-size" id="product-size" value="S">
                                             </li>
                                             <li class="list-inline-item"><span class="btn btn-success btn-size">S</span>
                                             </li>
@@ -216,6 +115,19 @@
 
 @section('js')
     <script src="{{ asset('js/slick.min.js') }}"></script>
+    <script>
+        $(function() {
+            $('.slider').slick({
+                slidesToShow: 3, // 一次顯示多少幻燈片
+                slidesToScroll: 1, // 一次滾動多少幻燈片
+                autoplay: true, // 啟用自動滾動
+                autoplaySpeed: 2000, // 自動滾動的速度（毫秒）
+                arrows: true, // 顯示前進/後退箭頭
+                dots: true // 顯示點指示器
+                // 可根據需要添加更多設置
+            });
+        })
+    </script>
 @endsection
 
 @section('css')
