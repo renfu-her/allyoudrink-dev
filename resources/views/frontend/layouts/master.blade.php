b7594e4b4f0644a93fb47bedd26940a1c12fcba0
Patrick Her <renfu.her@gmail.com>
2024-01-07 08:16:32 +0800

    update

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -164,17 +164,18 @@
     <!-- Start Banner Hero -->
     <div id="template-mo-zay-hero-carousel" class="carousel slide" data-bs-ride="carousel">
         <ol class="carousel-indicators">
-            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="0" class="active"></li>
-            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="1"></li>
-            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="2"></li>
+            @foreach ($banners as $key => $banner)
+                <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="{{ $key }}"
+                    @if ($key == 0) class="active" @endif></li>
+            @endforeach
         </ol>
         <div class="carousel-inner">
-            @foreach ($banners as $banner)
-                <div class="carousel-item">
+            @foreach ($banners as $key => $banner)
+                <div class="carousel-item @if ($key == 0) active @endif">
                     <div class="container">
                         <div class="row p-5">
                             <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                                <img class="object-fit-cover img-fluid" src="{{ asset('upload/banner/' . $banner->image) }}">
+                                <img class="object-fit-cover" src="{{ asset('upload/banner/' . $banner->image) }}">
                             </div>
                         </div>
                     </div>
