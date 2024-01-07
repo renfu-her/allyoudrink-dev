fa9960ec5d4cf24add346d9037a2f512fae6d9ad
Patrick Her <renfu.her@gmail.com>
2024-01-07 17:09:04 +0800

    update product index

--- app/Http/Controllers/ProductIndexController.php
+++ app/Http/Controllers/ProductIndexController.php
@@ -23,8 +23,8 @@ class ProductIndexController extends Controller
 
         $categories = ProductCategory::where('parent_id', 0)->with('children')->get();
         $banners = Banner::orderByDesc('sort', 1)->where('status', 1)->get();
-        
-        return view('frontend.product.product', compact('products', 'categories'));
+
+        return view('frontend.product.product', compact('products', 'categories', 'banners'));
     }
 
     public function detail(Request $request, $product_id)
