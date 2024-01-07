cc6e2e0b9b7955bb83443568e32ae5aa9cd81b73
Patrick Her <renfu.her@gmail.com>
2024-01-07 08:10:26 +0800

    update master

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -174,7 +174,7 @@
                     <div class="container">
                         <div class="row p-5">
                             <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                                <img class="img-fluid" src="{{ asset('upload/banner/' . $banner->image) }}">
+                                <img class="object-fit-cover" src="{{ asset('upload/banner/' . $banner->image) }}">
                             </div>
                         </div>
                     </div>
